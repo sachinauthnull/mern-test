901f2976348e38b0ff1f1d5555753793d335dbc8
sachinauthnull <sachin@authnull.com>
2024-04-01 17:24:21 +0530

    added frontend

---
+++ client/README.md
@@ -0,0 +1,3 @@
+## Frontend of the project
+
+this is the frontend of the project using nextjs as the framework.
---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='font-xl p-6'>Frontend</h1>
+    </div>
+  )
+}
